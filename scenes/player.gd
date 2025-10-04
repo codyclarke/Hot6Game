6f15b871177c4db76adcd4ed8d39e1a26f4732b9
Cody <clarke.cody@gmail.com>
2025-10-04 16:28:37 -0700

    Add some basic starter ALFs

---
+++ scenes/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@onready var stage = $"../"
+var health: int = 5
+var speed: float = 0.1
+@export var jump_speed: float = 20.0
+
+# when object is initialized
+func _init() -> void:
+	pass
+	
+# when object is loaded into a scene
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	position.x += speed * Input.get_axis("move_left", "move_right")
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed * -1
+	
+	
+func _physics_process(delta: float) -> void:
+	move_and_collide(velocity)
+	if !is_on_floor():
+		velocity.y += stage.gravity
---
+++ scenes/stage.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+var gravity : float = .98
