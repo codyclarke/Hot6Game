63328b33d98f471a83a2ee105e33cc6a1a000b40
Cody <clarke.cody@gmail.com>
2025-10-04 17:28:14 -0700

    Adjusted the movement to be contained in physics process

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var stage = $"../"
 var health: int = 5
-var speed: float = 0.1
+var speed: float = 200.0
 @export var jump_speed: float = 20.0
 
 # when object is initialized
@@ -13,14 +13,10 @@ func _init() -> void:
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
-	position.x += speed * Input.get_axis("move_left", "move_right")
-	if is_on_floor() && Input.is_action_just_pressed("jump"):
-		velocity.y = jump_speed * -1
-	
-	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 	if !is_on_floor():
 		velocity.y = min(velocity.y + stage.gravity, stage.max_velocity)
-		print('velocity: ', velocity)
+	position.x = lerp(position.x, position.x + speed * Input.get_axis("move_left", "move_right"),delta)
+	if is_on_floor() && Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed * -1
