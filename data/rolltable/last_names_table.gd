e6e182ecf44b93a0a29b239371998687eafb4bcf
Alexander Kahn <alexanderkahn@users.noreply.github.com>
2025-10-12 20:25:51 -0700

    Start splitting events and state from physics

--- data/rolltable/last_names_table.gd
+++ data/rolltable/last_names_table.gd
@@ -2,22 +2,32 @@ class_name LastNamesTable extends Node
 
 var _random = RandomNumberGenerator.new()
 var _names: Array[String] = [
+	"Antares",
+	"Book'Em",
 	"Carbonara",
 	"Caboose",
+	"Chainsaw",
 	"Delerium",
+	"Deluxe",
+	"Dingler",
 	"Fantastic",
 	"Ferocious",
 	"Freestyle",
+	"Glasseater",
 	"Horse",
 	"Inferno",
 	"Intensity",
 	"Omega",
+	"Mandalay",
 	"Manhattan",
 	"Mojito",
 	"Monsoon",
-	"Vandalay",
+	"Tarantula",
+	"Turtledove",
 	"Veracity",
-	"You-Know-It!"
+	"Whiplash",
+	"You-Know-It!",
+	"Zweihander",
 ]
 
 func roll() -> String:
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,5 @@
 extends CharacterBody2D
 
-signal turned_left(character_id: int)
-signal turned_right(character_id: int)
-
 @onready var stage = $"../"
 @onready var player_sprite = $AnimatedSprite2D
 @onready var name_label :Label = $NameLabel
@@ -10,7 +7,18 @@ signal turned_right(character_id: int)
 @export var character_data: CharacterData = PlayerData.new().generate()
 @export var projectile: PackedScene = preload("res://scenes/projectile.tscn")
 
-var character_direction :float = 0.0
+enum LookDirection { LEFT, RIGHT }
+enum ActionState { RUN, JUMP, IDLE }
+const _animations = {
+	ActionState.RUN : 'run',
+	ActionState.JUMP : 'jump',
+	ActionState.IDLE : 'idle' ,
+}
+
+var _look_direction: LookDirection = LookDirection.RIGHT
+var _move_speed: float = 0 # TODO: this probably duplicates velocity, which could be modified directly
+var _action_state: ActionState = ActionState.JUMP
+
 func _init() -> void:
 	pass
 	
@@ -23,26 +31,51 @@ func _process(_delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
-	character_direction = Input.get_axis("move_left", "move_right")
-	if !is_on_floor():
-		jump()
-	elif(character_direction<0):
-		player_sprite.play("run")
-		player_sprite.flip_h = true
-	elif character_direction>0:
-		player_sprite.play("run")
-		player_sprite.flip_h = false
-	else:
-		player_sprite.play("idle")
-	position.x = lerp(position.x, position.x + character_data.speed * character_direction,delta)
+	_update_state()
+	player_sprite.flip_h = true if _look_direction == LookDirection.LEFT else false
+	if _action_state == ActionState.JUMP:
+		velocity.y = min(velocity.y + stage.gravity, stage.max_velocity)
+	position.x = lerp(position.x, position.x + character_data.speed * _move_speed,delta)
+	player_sprite.play(_animations[_action_state])
+
+		
+func _update_state() -> void:
+	var input_speed = Input.get_axis("move_left", "move_right")
+	print(input_speed)
+	_action_state = _calculate_action_state(input_speed)
+	_look_direction = _calculate_look_direction(input_speed)
+	_move_speed = _calculate_move_speed(input_speed, _action_state)
+
+	
+func _calculate_action_state(input_speed: float) -> ActionState:
 	if is_on_floor() && Input.is_action_just_pressed("jump"):
 		velocity.y = character_data.jump_speed * -1
+		return ActionState.JUMP
+	elif !is_on_floor(): return ActionState.JUMP
+	elif input_speed == 0: return ActionState.IDLE
+	else: return ActionState.RUN
+	
+func _calculate_look_direction(input_speed: float) -> LookDirection:
+	if input_speed < 0:
+		return LookDirection.LEFT
+	elif input_speed > 0:
+		return LookDirection.RIGHT
+	else:
+		return _look_direction
+	
+func _calculate_move_speed(input_speed: float, action_state: ActionState) -> float:
+	match action_state:
+		ActionState.JUMP: 
+			return _move_speed
+		_: 
+			return input_speed
 
 func spawn_projectile():
 	var bang = projectile.instantiate()
-	bang.position = Vector2(position.x+(32*character_direction),position.y-8)
+	var start_position_offset = 1 if _look_direction == LookDirection.RIGHT else -1
+	bang.position = Vector2(position.x+(32*start_position_offset),position.y-8)
 	bang.scale *= 1.0-(randf()*0.2)-0.1
-	bang.direction = character_direction
+	bang.direction = start_position_offset
 	if bang.direction < 0:
 		bang.get_child(0).flip_h = true
 	owner.add_child(bang)
@@ -50,4 +83,3 @@ func spawn_projectile():
 func jump()->void:
 	velocity.y = min(velocity.y + stage.gravity, stage.max_velocity)
 	player_sprite.play("jump")
-	
