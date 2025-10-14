38ce058e9ff5d70ab52c207e994c501860b9b669
Cody <clarke.cody@gmail.com>
2025-10-14 15:58:56 -0700

    adjusted wall slide to use gravity, altered character_state to only emit on change

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,9 +5,10 @@ extends CharacterBody2D
 @onready var name_label: Label = $NameLabel
 @onready var gun_location: Node2D = $GunLocation
 @onready var character_state = $CharacterState
+@onready var wall_slide_speed: float = 3.0
+@onready var projectile: PackedScene = preload("res://scenes/projectile.tscn")
 
 @export var character_data: CharacterData = PlayerData.new().generate()
-@onready var projectile: PackedScene = preload("res://scenes/projectile.tscn")
 
 var start_position : Vector2
 
@@ -28,8 +29,11 @@ func _calculate_velocity(delta) ->  void:
 	velocity.x = int(lerp(velocity.x,Input.get_axis("move_left", "move_right") * character_data.speed,delta*10))
 	if (is_on_floor() || is_on_wall()) && Input.is_action_just_pressed("jump"):
 		velocity.y = character_data.jump_speed * -1
-		if is_on_wall(): 
+		if is_on_wall():
 			velocity.x = -character_data.speed*3 if character_state.look_direction == CharacterState.LookDirection.RIGHT else character_data.speed*3
+	elif is_on_wall():
+		velocity.y = min(velocity.y + stage.gravity/wall_slide_speed, stage.max_velocity)
+
 	elif is_on_floor():
 		velocity.y = 0
 	elif !is_on_wall():
